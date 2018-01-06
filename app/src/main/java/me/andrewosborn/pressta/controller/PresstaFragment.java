c60152559317090503eee151f4f2bed583817071
akosborn <andrewosborn93@gmail.com>
2018-01-06 15:11:11 -0600

    added click listener to quickbrew

--- app/src/main/java/me/andrewosborn/pressta/controller/PresstaFragment.java
+++ app/src/main/java/me/andrewosborn/pressta/controller/PresstaFragment.java
@@ -1,17 +1,21 @@
 package me.andrewosborn.pressta.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import me.andrewosborn.pressta.R;
 
 
 public class PresstaFragment extends Fragment
 {
+    private RelativeLayout mQuickBrewRelativeLayout;
+
     public static Fragment newInstance()
     {
         return new PresstaFragment();
@@ -26,6 +30,17 @@ public class PresstaFragment extends Fragment
         super.onCreateView(inflater, container, savedInstanceState);
 
         View view = inflater.inflate(R.layout.fragment_pressta, container, false);
+
+        mQuickBrewRelativeLayout = (RelativeLayout) view.findViewById(R.id.rel_layout_quick_brew);
+        mQuickBrewRelativeLayout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getActivity(), CalculationActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
