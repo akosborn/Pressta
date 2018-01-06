73b42f98866f7894eead53f62ac1db7eeeca0ddd
akosborn <andrewosborn93@gmail.com>
2018-01-06 13:48:59 -0600

    created home screen activity and its layout

---
+++ app/src/main/java/me/andrewosborn/pressta/controller/PresstaFragment.java
@@ -0,0 +1,32 @@
+package me.andrewosborn.pressta.controller;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.andrewosborn.pressta.R;
+
+
+public class PresstaFragment extends Fragment
+{
+    public static Fragment newInstance()
+    {
+        return new PresstaFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_pressta, container, false);
+
+        return view;
+    }
+}
