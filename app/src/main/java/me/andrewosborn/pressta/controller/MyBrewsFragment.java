fae1016acebcc0ba41f565efcf0b95df7c58dc0d
akosborn <andrewosborn93@gmail.com>
2018-02-11 21:00:51 -0600

    added labels to recyler views

--- app/src/main/java/me/andrewosborn/pressta/controller/MyBrewsFragment.java
+++ app/src/main/java/me/andrewosborn/pressta/controller/MyBrewsFragment.java
@@ -23,7 +23,6 @@ import javax.inject.Inject;
 import me.andrewosborn.pressta.PresstaApplication;
 import me.andrewosborn.pressta.R;
 import me.andrewosborn.pressta.model.Brew;
-import me.andrewosborn.pressta.model.Type;
 import me.andrewosborn.pressta.viewmodel.BrewListViewModel;
 
 public class MyBrewsFragment extends Fragment
@@ -35,8 +34,10 @@ public class MyBrewsFragment extends Fragment
 
     private BrewListViewModel mBrewListViewModel;
 
-    private RecyclerView mBrewRecyclerView;
-    private BrewAdapter mBrewAdapter;
+    private RecyclerView mHotBrewRecyclerView;
+    private HotBrewAdapter mHotBrewAdapter;
+    private RecyclerView mColdBrewRecyclerView;
+    private ColdBrewAdapter mColdBrewAdapter;
 
     private List<Brew> mBrews;
 
@@ -90,49 +91,62 @@ public class MyBrewsFragment extends Fragment
         View view = inflater.inflate(R.layout.fragment_my_brews, container, false);
 
         assignViews(view);
-        mBrewRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mHotBrewRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mColdBrewRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
     }
 
     private void setupUI()
     {
-        if (mBrewAdapter == null)
+        if (mHotBrewAdapter == null)
         {
-            mBrewAdapter = new BrewAdapter(mBrews);
-            mBrewRecyclerView.setAdapter(mBrewAdapter);
+            mHotBrewAdapter = new HotBrewAdapter(mBrews);
+            mHotBrewRecyclerView.setAdapter(mHotBrewAdapter);
         } else
         {
-            mBrewAdapter.setBrews(mBrews);
-            mBrewAdapter.notifyDataSetChanged();
+            mHotBrewAdapter.setBrews(mBrews);
+            mHotBrewAdapter.notifyDataSetChanged();
+        }
+
+        if (mColdBrewAdapter == null)
+        {
+            mColdBrewAdapter = new ColdBrewAdapter(mBrews);
+            mColdBrewRecyclerView.setAdapter(mColdBrewAdapter);
+        }
+        else
+        {
+            mColdBrewAdapter.setBrews(mBrews);
+            mColdBrewAdapter.notifyDataSetChanged();
         }
     }
 
     private void assignViews(View view)
     {
-        mBrewRecyclerView = view.findViewById(R.id.hot_brew_recycler_view);
+        mHotBrewRecyclerView = view.findViewById(R.id.hot_brew_recycler_view);
+        mColdBrewRecyclerView = view.findViewById(R.id.cold_brew_recycler_view);
     }
 
-    private class BrewAdapter extends RecyclerView.Adapter<BrewHolder>
+    private class HotBrewAdapter extends RecyclerView.Adapter<HotBrewHolder>
     {
         private List<Brew> mBrews;
 
-        BrewAdapter(List<Brew> brews)
+        HotBrewAdapter(List<Brew> brews)
         {
             mBrews = brews;
         }
 
         @Override
-        public BrewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        public HotBrewHolder onCreateViewHolder(ViewGroup parent, int viewType)
         {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             View view = layoutInflater.inflate(R.layout.list_item_brew, parent, false);
 
-            return new BrewHolder(view);
+            return new HotBrewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(BrewHolder holder, int position)
+        public void onBindViewHolder(HotBrewHolder holder, int position)
         {
             Brew brew = mBrews.get(position);
             holder.bindBrew(brew);
@@ -150,7 +164,44 @@ public class MyBrewsFragment extends Fragment
         }
     }
 
-    private class BrewHolder extends RecyclerView.ViewHolder
+    private class ColdBrewAdapter extends RecyclerView.Adapter<ColdBrewHolder>
+    {
+        private List<Brew> mColdBrews;
+
+        ColdBrewAdapter(List<Brew> coldBrews)
+        {
+            mColdBrews = coldBrews;
+        }
+
+        @Override
+        public ColdBrewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_item_brew, parent, false);
+
+            return new ColdBrewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ColdBrewHolder holder, int position)
+        {
+            Brew brew = mColdBrews.get(position);
+            holder.bindBrew(brew);
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return mBrews.size();
+        }
+
+        void setBrews(List<Brew> brews)
+        {
+            mBrews = brews;
+        }
+    }
+
+    private class HotBrewHolder extends RecyclerView.ViewHolder
         implements View.OnClickListener
     {
         private Brew mBrew;
@@ -164,7 +215,7 @@ public class MyBrewsFragment extends Fragment
         private TextView mWaterWeightTextView;
         private LinearLayout mHoursLinLayout;
 
-        BrewHolder(View itemView)
+        HotBrewHolder(View itemView)
         {
             super(itemView);
 
@@ -183,15 +234,78 @@ public class MyBrewsFragment extends Fragment
         @Override
         public void onClick(View view)
         {
-            if (mBrew.getType().equals(Type.HOT))
+            Intent intent = HotBrewActivity.newIntent(getContext(), mBrew.getId());
+            startActivity(intent);
+        }
+
+        void bindBrew(Brew brew)
+        {
+            mBrew = brew;
+
+            int minutes;
+            int seconds;
+            int brewDuration = mBrew.getBrewDuration();
+            int hours = brewDuration / 3600;
+
+            if (hours > 0)
             {
-                Intent intent = HotBrewActivity.newIntent(getContext(), mBrew.getId());
-                startActivity(intent);
+                minutes = (brewDuration % 3600) / 60;
+                seconds = minutes / 60;
+
+                mHoursLinLayout.setVisibility(View.VISIBLE);
+                mHoursTextView.setText(getString(R.string.hours, hours));
             }
-            else if (mBrew.getType().equals(Type.COLD))
+            else
             {
-                // TODO: 2/11/2018 Create intent for ColdBrewActivity
+                minutes = brewDuration / 60;
+                seconds = brewDuration % 60;
             }
+
+            mMinutesTextView.setText(getString(R.string.minutes, minutes));
+            mSecondsTextView.setText(getString(R.string.seconds, seconds));
+            mTitleTextView.setText(brew.getTitle());
+            mRatioTextView.setText(getString(R.string.ratio, mBrew.getRatio()));
+            mCoffeeWeightTextView.setText(String.valueOf(mBrew.getCoffeeWeight()));
+            mWaterWeightTextView.setText(String.valueOf(mBrew.getWaterWeight()));
+        }
+    }
+
+    private class ColdBrewHolder extends RecyclerView.ViewHolder
+        implements View.OnClickListener
+    {
+        private Brew mBrew;
+
+        private TextView mTitleTextView;
+        private TextView mHoursTextView;
+        private TextView mMinutesTextView;
+        private TextView mSecondsTextView;
+        private TextView mRatioTextView;
+        private TextView mCoffeeWeightTextView;
+        private TextView mWaterWeightTextView;
+        private LinearLayout mHoursLinLayout;
+
+        ColdBrewHolder(View itemView)
+        {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.text_view_title);
+            mHoursTextView = (TextView) itemView.findViewById(R.id.text_view_hours);
+            mMinutesTextView = (TextView) itemView.findViewById(R.id.text_view_minutes);
+            mSecondsTextView = (TextView) itemView.findViewById(R.id.text_view_seconds);
+            mRatioTextView = (TextView) itemView.findViewById(R.id.text_view_ratio);
+            mCoffeeWeightTextView = (TextView) itemView.findViewById(R.id.text_view_coffee_weight);
+            mWaterWeightTextView = (TextView) itemView.findViewById(R.id.text_view_water_weight);
+            mHoursLinLayout = (LinearLayout) itemView.findViewById(R.id.lin_layout_hours);
+        }
+
+        @Override
+        public void onClick(View view)
+        {
+            // TODO: 2/11/2018 Setup ColdBrewActivity.newIntent()
+//            Intent intent = ColdBrewActivity.newIntent(getContext(), mBrew.getId());
+//            startActivity(intent);
         }
 
         void bindBrew(Brew brew)
